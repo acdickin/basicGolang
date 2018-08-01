adcb37a0067e5b2c0797f1c33173e426fc5e00b3
Andrew Dickinson <andrew.dickinson@t2.local>
2018-08-01 11:06:15 -0700

    simple http server added

---
+++ httpServer.go
@@ -0,0 +1,23 @@
+package main
+import(
+  "fmt"
+  "net/http"
+)
+type Hello struct{}
+
+func(h Hello) ServeHTTP(w http.ResponseWriter, r *http.Request){
+  fmt.Fprint(w,"<h1>Hello from go web server!</h1>")
+
+}
+
+func main(){
+  var h Hello
+  fmt.Println("Running on localhost:4000")
+  err:= http.ListenAndServe("localhost:4000", h)
+  checkError(err)
+}
+func checkError(err error){
+  if err != nil{
+    panic(err)
+  }
+}
